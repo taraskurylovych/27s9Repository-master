00c907d37328234323719fa493159e599a2339e8
Taras Kurylovych <taras.kurylovych@gmail.com>
2017-10-01 10:18:51 +0300

    Lesson 5 templateUrl

--- WebSite/src/app/app.component.js
+++ WebSite/src/app/app.component.js
@@ -8,14 +8,14 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular 2';
+        this.name = 'Angular 2 - sdfsdaf';
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "<h1>Hello {{name}}</h1>",
+        templateUrl: 'app/app.component.html',
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
